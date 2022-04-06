bfb04ba277a831286a92a90cd1c0fb92ce58f8a0
davegrnr <davegrnr@gmail.com>
2022-04-06 14:20:39 -0400

    Practice for sections 1-6

---
+++ frequency-counter.js
@@ -0,0 +1,22 @@
+function frequencyCounter(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let char of str1.toLowerCase()) {
+    frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+  }
+  for (let char of str2.toLowerCase()) {
+    frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(frequencyCounter("pizza jazz", "azzj azzip"));
